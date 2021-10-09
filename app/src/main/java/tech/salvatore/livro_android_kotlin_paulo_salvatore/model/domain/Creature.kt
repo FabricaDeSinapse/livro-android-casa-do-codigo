7b9e62a1cc566bd18ec2bc295de555da1d694722
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-09 16:01:19 -0300

    feat(creature): add optional data to domain structure

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
@@ -1,13 +1,12 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain
 
 data class Creature(
-    val id: Int,
     val number: Int,
     val name: String,
-    val imageUrl: String,
-    val hungry: Int,
-    val strength: Int,
-    val humor: Int,
+    val imageUrl: String?,
+    val hungry: Int = 0,
+    val strength: Int = 0,
+    val humor: Int = 0,
     val evolveTo: Creature? = null,
     val users: List<User> = emptyList()
 )
