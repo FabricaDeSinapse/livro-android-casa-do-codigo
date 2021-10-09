dbb8eec5a5f1ef5bed1b174a4fe779d3d56eeb24
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-09 00:28:22 -0300

    refactor(model): rename files

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/CreatureLocalDataSource.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/CreatureLocalDataSource.kt
@@ -1,3 +1,3 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local
 
-class CreaturesLocalDataSource
+class CreatureLocalDataSource
--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/remote/api/services/CreatureService.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/remote/api/services/CreatureService.kt
@@ -1,3 +1,3 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.remote.api.services
 
-interface CreaturesService
+interface CreatureService
