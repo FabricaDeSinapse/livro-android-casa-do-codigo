f47e1948b68ca4a054eb02861a49a5518d41db8f
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-09 15:06:44 -0300

    feat(creature): creature's replay subject should replay only last call

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/repository/CreatureRepository.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/repository/CreatureRepository.kt
@@ -1,6 +1,7 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository
 
 import android.app.Application
+import android.util.Log
 import io.reactivex.rxjava3.subjects.ReplaySubject
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain.Creature
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local.CreatureLocalDataSource
@@ -15,7 +16,7 @@ class CreatureRepository(application: Application) {
     // TODO: Maybe should be singleton
     private val remoteDataSource = CreatureRemoteDataSource
 
-    val creatures = ReplaySubject.create<List<Creature>>()
+    val creatures = ReplaySubject.create<List<Creature>>(1)
 
     init {
         // Load Creatures From Local Data Source
@@ -23,16 +24,20 @@ class CreatureRepository(application: Application) {
             // Convert CreatureEntity to Creature
             emptyList<Creature>()
         }.subscribe {
+            Log.d("CREATURE", "Finish loading local data source.")
+
             // Update creatures' ReplaySubject
             creatures.onNext(it)
         }
 
         // Load Creatures From Local Remote Source
         remoteDataSource.creatures.map {
-            // Convert CreatureApiResponse to Creature
+            // Convert CreatureApiResponse to CreatureEntity
             emptyList<Creature>()
             // TODO: Insert creatures on database (by number)
         }.subscribe {
+            Log.d("CREATURE", "Finish loading remote data source.")
+
             // Update creatures' ReplaySubject
             creatures.onNext(it)
         }
