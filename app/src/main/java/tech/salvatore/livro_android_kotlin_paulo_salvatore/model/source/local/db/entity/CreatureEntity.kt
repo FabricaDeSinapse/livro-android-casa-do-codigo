c16cdc1c36adacb5436bf9fc5ef02c4498ba5fbf
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-09 15:00:20 -0300

    feat(creature): add unique index to `number` and `name` columns

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/db/entity/CreatureEntity.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/db/entity/CreatureEntity.kt
@@ -1,9 +1,15 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "creature")
+@Entity(
+    tableName = "creature", indices = [
+        Index(value = ["number"], unique = true),
+        Index(value = ["name"], unique = true)
+    ]
+)
 data class CreatureEntity(
     @PrimaryKey
     val id: Int,
