45afb4b57f5ebcf443d392bf4c63da16931ee658
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-11 00:33:29 -0300

    feat(creatures): hide `MutableLiveData` and dispose subscription `onCleared`

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreaturesViewModel.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreaturesViewModel.kt
@@ -2,21 +2,30 @@ package tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.extensions.rx.CompositeDisposableExtensions.plusAssign
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain.Creature
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.CreatureRepository
 
 class CreaturesViewModel(application: Application) : AndroidViewModel(application) {
     private val repository = CreatureRepository(application)
 
-    val creatures = MutableLiveData<List<Creature>>()
+    private val _creatures = MutableLiveData<List<Creature>>()
 
-    // TODO: make only LiveData available outside ViewModel
+    val creatures: LiveData<List<Creature>>
+        get() = _creatures
+
+    private val composite = CompositeDisposable()
 
     init {
-        // TODO: remove subscription
-        repository.creatures.subscribe {
-            creatures.postValue(it)
+        composite += repository.creatures.subscribe {
+            _creatures.postValue(it)
         }
+    }
+
+    override fun onCleared() {
+        composite.dispose()
     }
 }
