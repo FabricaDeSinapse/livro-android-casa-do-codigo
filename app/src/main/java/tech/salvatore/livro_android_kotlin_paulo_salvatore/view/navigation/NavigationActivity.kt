bbda952faa2549bfec1704816ee5f7fd49a2f756
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-11 23:55:00 -0300

    chore(navigation): remove live data observation to prevent unexpected behaviors

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/navigation/NavigationActivity.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/navigation/NavigationActivity.kt
@@ -24,15 +24,15 @@ class NavigationActivity : AppCompatActivity() {
         val navGraph = graphInflater.inflate(R.navigation.nav_graph)
 
         // TODO: Maybe would be better to observe only creatures' count instead of all list
-        creaturesViewModel.creatures.observe(this, {
-            val destination = when (it.count()) {
-                0 -> R.id.creatures_choose_dest
-                else -> R.id.creatures_list_dest
-            }
+        val creaturesCount = creaturesViewModel.creatures.value?.count() ?: 0
 
-            navGraph.setStartDestination(destination)
+        val destination = when (creaturesCount) {
+            0 -> R.id.creatures_choose_dest
+            else -> R.id.creatures_list_dest
+        }
 
-            navController.graph = navGraph
-        })
+        navGraph.setStartDestination(destination)
+
+        navController.graph = navGraph
     }
 }
