f763b15676cdf0b6851910b51d481e949409c6b0
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-24 23:59:17 -0300

    refactor(di): add `RxDataStore` and change methods to return inline

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/datastore/UserSessionManager.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/datastore/UserSessionManager.kt
@@ -1,12 +1,9 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local.datastore
 
-import android.content.Context
 import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder
 import androidx.datastore.rxjava3.RxDataStore
-import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
@@ -14,11 +11,8 @@ import javax.inject.Singleton
 
 @Singleton
 class UserSessionManager @Inject constructor(
-    @ApplicationContext context: Context
+    private val sessionDataStore: RxDataStore<Preferences>
 ) {
-    private val sessionDataStore: RxDataStore<Preferences> =
-        RxPreferenceDataStoreBuilder(context, "SESSION_DATA").build()
-
     private object SessionKeys {
         val ACTIVE_USER = longPreferencesKey("ACTIVE_USER")
     }
--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/di/LocalDataSourceModule.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/di/LocalDataSourceModule.kt
@@ -3,6 +3,9 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local.di
 
 import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.rxjava3.RxPreferenceDataStoreBuilder
+import androidx.datastore.rxjava3.RxDataStore
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
@@ -17,17 +20,15 @@ import javax.inject.Singleton
 object LocalDataSourceModule {
     @Singleton
     @Provides
-    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
-        return Room.databaseBuilder(
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase =
+        Room.databaseBuilder(
             context.applicationContext,
             AppDatabase::class.java,
             "app-database"
         ).build()
-    }
 
-//    @Singleton
-//    @Provides
-//    fun provideRxDataStore(@ApplicationContext context: Context): RxDataStore<Preferences> {
-//        return RxPreferenceDataStoreBuilder(context, "SESSION_DATA").build()
-//    }
+    @Singleton
+    @Provides
+    fun provideRxDataStore(@ApplicationContext context: Context): RxDataStore<Preferences> =
+        RxPreferenceDataStoreBuilder(context, "SESSION_DATA").build()
 }
