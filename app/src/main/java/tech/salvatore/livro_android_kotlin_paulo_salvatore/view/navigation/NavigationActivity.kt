c0216bfe8d0feaa65f5c2d60b0a1760ae715cdef
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-25 00:49:02 -0300

    feat(navigation): connect splash screen load with users and creatures loading

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/navigation/NavigationActivity.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/navigation/NavigationActivity.kt
@@ -1,36 +1,69 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.view.navigation
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.R
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.view.creatures.list.CreaturesListFragmentDirections
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.NavigationViewModel
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.UserViewModel
 
 @AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
+    private val navigationViewModel: NavigationViewModel by viewModels()
+
     private val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
 
+        val content = findViewById<View>(android.R.id.content)
+
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    val isReady = navigationViewModel.isReady.value != null
+
+                    if (isReady && initNavigation()) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+
+                        return true
+                    }
+
+                    return false
+                }
+            }
+        )
+    }
+
+    private fun initNavigation(): Boolean {
+        // Load NavController
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Add NavGraph
         val graphInflater = navHostFragment.navController.navInflater
         val navGraph = graphInflater.inflate(R.navigation.nav_graph)
         navController.graph = navGraph
 
-        userViewModel.user.observe(this, {
-            if (it.newCreaturesAvailable > 0
-                && navController.currentDestination?.id != R.id.creatures_choose_dest
-            ) {
-                val action = CreaturesListFragmentDirections.creaturesChooseAction()
-                navController.navigate(action)
-            }
-        })
+        // Make sure user is available beforing releasing splash screen
+        val user = userViewModel.user.value ?: return false
+
+        // Check if any creatures are available and navigate to choose creatures screen
+        if (user.newCreaturesAvailable > 0
+            && navController.currentDestination?.id != R.id.creatures_choose_dest
+        ) {
+            val action = CreaturesListFragmentDirections.creaturesChooseAction()
+            navController.navigate(action)
+        }
+
+        // Finish navigation init
+        return true
     }
 }
---
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/NavigationViewModel.kt
@@ -0,0 +1,39 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.extensions.rx.CompositeDisposableExtensions.plusAssign
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.CreatureRepository
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.UserRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class NavigationViewModel @Inject constructor(
+    userRepository: UserRepository,
+    creatureRepository: CreatureRepository
+) : ViewModel() {
+    private val _isReady = MutableLiveData<Boolean>()
+    val isReady: LiveData<Boolean>
+        get() = _isReady
+
+    private val composite = CompositeDisposable()
+
+    init {
+        composite += Observable.combineLatest(
+            userRepository.user,
+            creatureRepository.creatures
+        ) { _, creatures ->
+            creatures.count() > 0
+        }.subscribe {
+            _isReady.postValue(it)
+        }
+    }
+
+    override fun onCleared() {
+        composite.dispose()
+    }
+}
