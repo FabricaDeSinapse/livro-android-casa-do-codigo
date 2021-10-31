083bc7990e7aace1dceaebfa4ff2ebe220e9fab7
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-31 06:40:31 -0300

    chore(creatures): add ToDos and timestamp temporary function

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
@@ -1,5 +1,17 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain
 
+import java.text.DateFormat
+import java.util.*
+
+// Hungry consts
+const val maxHungry = 5
+const val hungryPercentagePerPoint = 100 / maxHungry
+
+fun currentTimestamp(): String =
+    DateFormat.getTimeInstance().apply {
+        timeZone = TimeZone.getTimeZone("gmt")
+    }.format(Date())
+
 data class Creature(
     val number: Long,
     val name: String,
@@ -9,8 +21,14 @@ data class Creature(
     val type1: CreatureType? = null,
     val type2: CreatureType? = null,
 
-    val hungry: Int = 1,
+    // TODO: Talvez remover hungry e usar apenas o lastFeed para definir
+    // TODO: Botão só aparece quando está com 4 ou 5 e sempre zera a fome
+    val hungry: Int = 0,
+
+    // TODO: Sobe 1 sempre que treina, só pode treinar 1x a cada 2 minutos
     val strength: Int = 0,
+
+    // TODO: Sobe 1 sempre que brinca, só pode brincar 1x a cada 3 minutos
     val humor: Int = 0,
 
     val lastFeed: Int = 0,
@@ -20,7 +38,15 @@ data class Creature(
     val evolveTo: Creature? = null,
     val users: List<User> = emptyList()
 ) {
-    val canFeed = hungry > 0
-    val canTrain = strength < 5
-    val canPlay = humor < 5
+    val canFeed: Boolean get() = hungry > 0
+    val canTrain: Boolean get() = strength < 5
+    val canPlay: Boolean get() = humor < 5
+
+    // TODO: Improve
+    val hungryPercent: Int
+        get() = if (hungry > 0) {
+            hungry * hungryPercentagePerPoint
+        } else {
+            0
+        }
 }
