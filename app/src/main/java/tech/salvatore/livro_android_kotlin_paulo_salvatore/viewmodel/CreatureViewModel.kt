4199b3dc32c58b384acb2a60424affb301f24e66
Paulo Salvatore <salvatoregames@gmail.com>
2021-11-21 16:05:31 -0300

    chore(creature): add `UserCreatureRepository` and feed creature logic

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreatureViewModel.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreatureViewModel.kt
@@ -9,11 +9,13 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.extensions.rx.CompositeDisposableExtensions.plusAssign
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain.Creature
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.CreatureRepository
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.UserCreatureRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class CreatureViewModel @Inject constructor(
-    private val repository: CreatureRepository
+        private val creatureRepository: CreatureRepository,
+        private val userCreatureRepository: UserCreatureRepository
 ) : ViewModel() {
     val number = MutableLiveData<Long>()
 
@@ -25,16 +27,22 @@ class CreatureViewModel @Inject constructor(
     private val composite = CompositeDisposable()
 
     fun loadCreature(creatureId: Long) {
-        composite += repository
-            .findByNumber(creatureId)
-            .subscribe {
-                _creature.postValue(it)
-            }
+        composite += creatureRepository
+                .findByNumber(creatureId)
+                .subscribe {
+                    _creature.postValue(it)
+                }
     }
 
     val feed: Function0<Unit> = {
         Log.d("CREATURE", "Feed creature")
     }
+
+    /*
+    feed creature
+    add exp to creature (update on db)
+    set lastFeed as current time
+    */
 
     val play: Function0<Unit> = {
         Log.d("CREATURE", "Play with creature")
