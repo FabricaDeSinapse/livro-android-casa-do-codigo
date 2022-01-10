85d87a32e3c6a91ac806b29c1fdb7b5aac5c5024
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-10 00:27:10 -0300

    feat(creatures-added): implement button to go back after adding a creature

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/creatures/added/CreaturesAddedFragment.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/creatures/added/CreaturesAddedFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.R
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.databinding.CreaturesAddedFragmentBinding
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.view.creatures.view.CreaturesViewFragmentArgs
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.CreatureViewModel
@@ -34,6 +37,11 @@ class CreaturesAddedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btAddCreature = view.findViewById<Button>(R.id.btAddCreature)
+        btAddCreature.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         val safeArgs: CreaturesViewFragmentArgs by navArgs()
         val creatureId = safeArgs.creatureNumber
