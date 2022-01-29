cd6d916b28aa289c73bca8594bd808a1a82e6651
Paulo Salvatore <salvatoregames@gmail.com>
2022-01-29 18:24:24 -0300

    fix(creatures-evolving): fix `ViewModel` reference

--- app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/creatures/evolving/CreaturesEvolvingFragment.kt
+++ app/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/creatures/evolving/CreaturesEvolvingFragment.kt
@@ -8,13 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.databinding.CreaturesEvolvingFragmentBinding
-import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.CreaturesViewModel
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.CreatureViewModel
 
 @AndroidEntryPoint
 class CreaturesEvolvingFragment : Fragment() {
     private lateinit var binding: CreaturesEvolvingFragmentBinding
 
-    private val viewModel: CreaturesViewModel by viewModels()
+    private val viewModel: CreatureViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
