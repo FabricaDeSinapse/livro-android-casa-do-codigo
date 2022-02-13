91b6453e42ec5114a51a04500e60391fc201fc70
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-13 17:26:48 -0300

    chore(modules): create modules `step01-initial` & `my-project`

---
+++ step01-initial/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationActivity.kt
@@ -0,0 +1,11 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
