c1d4f189eaa3463383fdac7bca49dbded20fb850
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-24 01:15:33 -0300

    feat(step1): create app

--- step01-initial/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationActivity.kt
+++ step01-initial/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationActivity.kt
@@ -1,9 +1,14 @@
 package tech.salvatore.livro_android_kotlin_paulo_salvatore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
+    private val navigationViewModel: NavigationViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
---
+++ step01-initial/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/NavigationViewModel.kt
@@ -0,0 +1,7 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class NavigationViewModel : ViewModel()
