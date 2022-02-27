c238417590749ccdc113bffbc508f74d5af1b1b4
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-26 23:53:37 -0300

    step3: add model files

---
+++ step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/repository/CreaturesRepository.kt
@@ -0,0 +1,15 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository
+
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain.Creature
+
+class CreaturesRepository {
+    val creatures: List<Creature>
+
+    init {
+        val creature1 = Creature(1, "Java", "https://i.imgur.com/PHfd1h2.png")
+        val creature2 = Creature(2, "Kotlin", "https://i.imgur.com/EZ3zCBp.png")
+        val creature3 = Creature(3, "Android", "https://i.imgur.com/r4UjONq.png")
+
+        creatures = listOf(creature1, creature2, creature3)
+    }
+}
