c85d309b2d430dd1011540cda0895bdc2bd09fc7
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-27 17:57:09 -0300

    step3: implement recycler view

--- step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/CreaturesListFragment.kt
+++ step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/CreaturesListFragment.kt
@@ -6,9 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.R
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.CreaturesViewModel
 
+@AndroidEntryPoint
 class CreaturesListFragment : Fragment() {
     private val creaturesViewModel: CreaturesViewModel by viewModels()
 
@@ -20,8 +24,13 @@ class CreaturesListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
         creaturesViewModel.creatures.observe(viewLifecycleOwner) {
-            // it = List<Creature>
+            recyclerView.adapter = CreaturesListAdapter(it)
         }
     }
 }
