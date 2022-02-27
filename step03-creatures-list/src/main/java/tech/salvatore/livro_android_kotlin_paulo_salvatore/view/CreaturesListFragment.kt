9593dcd43879e2ae6132d4d3604f99a93ab02b47
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-27 02:32:18 -0300

    step3: add livedata observe

--- step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/CreaturesListFragment.kt
+++ step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/view/CreaturesListFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.R
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.viewmodel.CreaturesViewModel
 
 class CreaturesListFragment : Fragment() {
+    private val creaturesViewModel: CreaturesViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.creatures_list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        creaturesViewModel.creatures.observe(viewLifecycleOwner) {
+            // it = List<Creature>
+        }
     }
 }
