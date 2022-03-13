8b8abd10b9f3d58a2bc573013c9d3b6437df90a8
Paulo Salvatore <salvatoregames@gmail.com>
2022-03-13 14:52:57 -0300

    chore: update creature list

--- step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/repository/CreaturesRepository.kt
+++ step03-creatures-list/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/repository/CreaturesRepository.kt
@@ -11,8 +11,32 @@ class CreaturesRepository @Inject constructor() {
     init {
         val creature1 = Creature(1, "Java", "https://i.imgur.com/PHfd1h2.png")
         val creature2 = Creature(2, "Kotlin", "https://i.imgur.com/EZ3zCBp.png")
-        val creature3 = Creature(3, "Android", "https://i.imgur.com/r4UjONq.png")
+        val creature3 = Creature(3, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature4 = Creature(4, "C", "https://i.imgur.com/iWeXxA9.png")
+        val creature5 = Creature(5, "C++", "https://i.imgur.com/YYkSF5A.png")
+        val creature6 = Creature(6, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature7 = Creature(7, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature8 = Creature(8, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature9 = Creature(9, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature10 = Creature(10, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature11 = Creature(11, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature12 = Creature(12, "????", "https://i.imgur.com/rNKVxSt.png")
+        val creature13 = Creature(13, "????", "https://i.imgur.com/rNKVxSt.png")
 
-        creatures = listOf(creature1, creature2, creature3)
+        creatures = listOf(
+            creature1,
+            creature2,
+            creature3,
+            creature4,
+            creature5,
+            creature6,
+            creature7,
+            creature8,
+            creature9,
+            creature10,
+            creature11,
+            creature12,
+            creature13,
+        )
     }
 }
