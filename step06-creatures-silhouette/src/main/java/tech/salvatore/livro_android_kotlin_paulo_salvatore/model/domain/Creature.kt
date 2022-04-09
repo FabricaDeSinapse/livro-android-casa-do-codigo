89db4bdfb90fb700bdb1b11eee9264bee9d9c433
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-09 20:59:20 -0300

    feat(step6): implement silhouette

--- step06-creatures-silhouette/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
+++ step06-creatures-silhouette/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
@@ -4,4 +4,5 @@ data class Creature(
     val number: Int,
     val name: String,
     val imageUrl: String,
+    val isKnown: Boolean = true,
 )
--- step06-creatures-silhouette/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreaturesViewModel.kt
+++ step06-creatures-silhouette/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/viewmodel/CreaturesViewModel.kt
@@ -5,16 +5,27 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain.Creature
 import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.CreaturesRepository
+import tech.salvatore.livro_android_kotlin_paulo_salvatore.model.repository.UserRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class CreaturesViewModel @Inject constructor(
-    private val creaturesRepository: CreaturesRepository
+    private val creaturesRepository: CreaturesRepository,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
     val creatures = MutableLiveData<List<Creature>>()
 
     init {
-        creatures.value = creaturesRepository.creatures
+        creatures.value = creaturesRepository.creatures.map {
+            val isKnown = userRepository.user.creatures.any { creatureOwnByUser ->
+                creatureOwnByUser.number == it.number
+            }
+
+            it.copy(
+                name = if (isKnown) it.name else "?????",
+                isKnown = isKnown
+            )
+        }
     }
 
     fun findCreature(number: Int) = creaturesRepository.findCreature(number)
