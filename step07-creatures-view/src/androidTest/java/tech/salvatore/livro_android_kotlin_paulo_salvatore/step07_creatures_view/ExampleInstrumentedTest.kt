05d63d776df73e4dfaa78ead40f314bb6b880b60
Paulo Salvatore <salvatoregames@gmail.com>
2022-05-18 00:26:59 -0300

    chore(step7): create module

---
+++ step07-creatures-view/src/androidTest/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/step07_creatures_view/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package tech.salvatore.livro_android_kotlin_paulo_salvatore.step07_creatures_view
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals(
+            "tech.salvatore.livro_android_kotlin_paulo_salvatore.step07_creatures_view",
+            appContext.packageName
+        )
+    }
+}
