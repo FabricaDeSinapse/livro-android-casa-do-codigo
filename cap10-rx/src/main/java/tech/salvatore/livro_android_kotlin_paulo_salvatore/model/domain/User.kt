4581a8eed46078521eae00993eb4edd3d9988c4e
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-23 11:07:16 -0300

    chore: delete other chapters

--- cap10-rx/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/User.kt
+++
@@ -1,8 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain
-
-data class User(
-    val name: String,
-
-    var hasCreatureAvailable: Boolean,
-    val creatures: MutableList<Creature> = mutableListOf(),
-)
