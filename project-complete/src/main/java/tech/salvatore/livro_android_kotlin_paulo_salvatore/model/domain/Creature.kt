c996c025562e8e3066dbea838be6e69245bb58d6
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-23 10:45:48 -0300

    chore: delete other chapters

--- project-complete/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/domain/Creature.kt
+++
@@ -1,8 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.domain
-
-data class Creature(
-    val number: Int,
-    val name: String,
-    val imageUrl: String,
-    val isKnown: Boolean = false,
-)
