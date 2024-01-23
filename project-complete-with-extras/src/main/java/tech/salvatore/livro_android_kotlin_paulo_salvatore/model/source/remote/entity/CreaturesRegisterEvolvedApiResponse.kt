3aee8714ffc7a1874467fc3d87cf19616edf52fe
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-23 11:00:34 -0300

    chore: delete other chapters

--- project-complete-with-extras/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/remote/entity/CreaturesRegisterEvolvedApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.remote.entity
-
-data class CreaturesRegisterEvolvedApiResponse(
-    val hungry: Int,
-    val strength: Int,
-    val humor: Int
-)
