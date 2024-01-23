afa0e2901ded4cad069a7d604f9ba3267d848aab
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-23 10:49:56 -0300

    chore: delete other chapters

--- project-complete-with-extras/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/model/source/local/datastore/UserSession.kt
+++
@@ -1,5 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore.model.source.local.datastore
-
-data class UserSession(
-    val activeUser: Long?
-)
--- project-complete-with-extras/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/utils/Optional.kt
+++
@@ -1,3 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore.utils
-
-data class Optional<M>(val value: M?)
