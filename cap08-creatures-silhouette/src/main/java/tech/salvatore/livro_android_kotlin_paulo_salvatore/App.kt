bbc69b54a3233c702ebe9b08f9cd54253dabafa7
Paulo Salvatore <salvatoregames@gmail.com>
2024-01-23 10:53:45 -0300

    chore: delete other chapters

--- cap08-creatures-silhouette/src/main/java/tech/salvatore/livro_android_kotlin_paulo_salvatore/App.kt
+++
@@ -1,7 +0,0 @@
-package tech.salvatore.livro_android_kotlin_paulo_salvatore
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()
